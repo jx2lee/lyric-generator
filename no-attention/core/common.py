047c6119bc3e87f0c5ba125177ee08d74bf0994a
jx2lee <jaejun.lee.1991@gmail.com>
2020-09-06 22:50:22 +0900

    data

--- no-attention/core/common.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-def print_info(string):
-    print('\033[31m \033[43m' + '[INFO]'+ '\033[0m' + ' ' + string)
-
-def print_error(string):
-    print('\033[37m \033[101m' + '[ERROR]'+ '\033[0m' + ' ' + string)
