3f08c5da5709ebbad5aafc968432063b8e45d7b7
jx2lee <jaejun.lee.1991@gmail.com>
2020-09-06 22:52:38 +0900

    predict lyrics

---
+++ prediction.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# !/usr/bin/env python
+from core.generator import *
+from core.model import generator_graph
+from core.var import *
+import pickle
+
+if __name__ == '__main__':
+    dictionary, test_data = import_data(PKL_PATH)
+    prediction = generate_lyric(test_data, CHECKPOINTS_PATH)
+    print_lyric(prediction, dictionary)
